2034058e4acf9ba0967f9004f3ec89cbf8f34151
Suraj-Srinath <104007481+Suraj-Srinath@users.noreply.github.com>
2022-04-19 18:31:09 +0530

    Add files via upload

---
+++ helper_function.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+import os
+
+def view_random_image(target_dir, target_class):
+  # Setup target directory (we'll view images from here)
+  target_folder = os.path.join(target_dir,target_class)
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(os.path.join(target_folder,random_image[0]))
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+
+  print(f"Image shape: {img.shape}") # show the shape of the image
+
+  return img
+
+
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
+
+def pred_and_plot(model, filename, class_names):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  if len(pred[0]) > 1: # check for multi-class
+    pred_class = class_names[pred.argmax()] # if more than one output, take the max
+  else:
+    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+
+  # Plot the image and predicted class
+  plt.imshow(img)
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False);
+
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
